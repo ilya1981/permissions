052ca218aeed0ceb9ea75f980b2e91fd16add1cf
Ilya81 <ilya110881@gmail.com>
2025-07-22 18:10:08 +0300

    first commit

---
+++ api_with_restrictions/advertisements/views.py
@@ -0,0 +1,32 @@
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.viewsets import ModelViewSet
+from advertisements.permissions import IsOwnerPermission, IsAdminUserOnly
+from advertisements.models import Advertisement
+from advertisements.serializers import AdvertisementSerializer
+from rest_framework.decorators import action
+
+
+class AdvertisementViewSet(ModelViewSet):
+    """ViewSet для объявлений."""
+
+    # TODO: настройте ViewSet, укажите атрибуты для кверисета,
+    #   сериализаторов и фильтров
+    queryset =  Advertisement.objects.all()
+    serializer_class = AdvertisementSerializer
+    filter_backends = [DjangoFilterBackend, SearchFilter]
+    filters_fields = ["creator","status", "created_at",]
+
+
+
+    def perform_create(self, serializer):
+        serializer.save(creator=self.request.user)
+
+    def get_permissions(self):
+        """Получение прав для действий."""
+        if self.action in ["create"]:
+            return [IsAuthenticated()]
+        elif self.action in ["update", "destroy", "partial_update"]:
+            return [IsOwnerPermission(), IsAdminUserOnly()]
+        return []
